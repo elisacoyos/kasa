e1ad7999b030427ffcf8ca04090576a10209f752
ElisaCoyos <elisacoyos@gmail.com>
2024-01-05 14:59:37 +0100

    commit

--- src/Router.jsx
+++ src/Router.jsx
@@ -8,7 +8,7 @@ import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import About from "./pages/About";
 import ApartmentPage from "./pages/ApartmentPage";
-import Error from "./pages/error"; 
+import Error from "./pages/Error"; 
 
 
   function Router () {
