04885c852886672113a9c72970b1e8f9ce100b77
ElisaCoyos <elisacoyos@gmail.com>
2024-01-08 17:46:55 +0100

    page home avec json

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.scss';
+import { Link } from 'react-router-dom';
+
+
+export default function Card({ id, title, cover }) {
+
+	return (
+		<Link to={`/logement/${id}`} className='card'>
+			<h3 className='card__title'>{title}</h3>
+			<img className='card__picture' src={cover} alt={title} />
+		</Link>
+	);
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,26 @@
-import { Fragment } from "react";
-import imgBanner from '../assets/banner-img.png'
+import React, { useEffect, Fragment } from "react";
+import imgBanner from '../assets/banner-img.png';
 import Banner from "../components/Banner";
-import ApartmentGrid from "../components/ApartmentGrid";
-
+import Card from "../components/Card"; // Asegúrate de que este es el nombre correcto
+import logementData from '../assets/data/logementData.json'; // Asegúrate de que la ruta de importación sea correcta
+import './Home.scss';
+ 
 
 function Home() {
+    useEffect(() => {
+        document.title = `Kasa | Accueil`;
+    }, []);
+
     return (
         <Fragment>
-        <Banner text="Chez vous, partout et ailleurs" img={imgBanner} />
-        <ApartmentGrid />
+            <Banner img={imgBanner} text="Chez vous, partout et ailleurs" />
+            <div className="gallery">
+                {logementData.map(({ id, title, cover }) => (
+                    <Card key={id} id={id} title={title} cover={cover} />
+                ))}
+            </div>
         </Fragment>
-    )
-    
+    );
 }
 
 export default Home;
