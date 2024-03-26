06da016094046442133d238c77dd2610eed54cc4
elisacoyos <elisacoyos@gmail.com>
2024-03-26 17:05:37 +0100

    run buil

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Utiliza la ruta base '/kasa/' para producción, pero usa '/' para desarrollo local.
+const base = process.env.NODE_ENV === 'production' ? '/kasa/' : '/';
+
 export default defineConfig({
+  base,
   plugins: [react()],
-})
+});
